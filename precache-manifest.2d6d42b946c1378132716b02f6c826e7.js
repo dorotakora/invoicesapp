eef2dbcbe44c71cc1c93a429dc4b7e6cb4b70974
dorotakora <dorota.kora@o2.pl>
2019-05-11 17:12:59 +0200

    Updates

---
+++ precache-manifest.2d6d42b946c1378132716b02f6c826e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f47ed99abc1381ae10c0fb12cdea01",
+    "url": "/invoicesapp/index.html"
+  },
+  {
+    "revision": "40922a9dbdd020b22efd",
+    "url": "/invoicesapp/static/css/main.e69f3637.chunk.css"
+  },
+  {
+    "revision": "7c908abdb9ecdee964df",
+    "url": "/invoicesapp/static/js/2.72a7f0a7.chunk.js"
+  },
+  {
+    "revision": "40922a9dbdd020b22efd",
+    "url": "/invoicesapp/static/js/main.2b326c4f.chunk.js"
+  },
+  {
+    "revision": "e79c1c22b287d817f5c7",
+    "url": "/invoicesapp/static/js/runtime~main.ca263340.js"
+  }
+]);
